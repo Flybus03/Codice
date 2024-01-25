6a303d9134beba1c79c6b04bbbff7fc01ca8c9a7
Paperagialla <mattiabusato1234@gmail.com>
2024-01-25 23:57:06 +0100

    Aggiunti tutti i pop-up nella default-page
    Mancano ancora delle funzioni, ma i pop-up sono tutti presenti

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+/* TODO: fixa sta merda che non si capisce niente */
+
 const openRegister = document.getElementById('openRegister');
 const closeRegister = document.getElementById('closeRegister');
+
 const openLogin = document.getElementById('openLogin');
 const closeLogin = document.getElementById('closeLogin');
+
+const openVerificaEmail = document.getElementById('openVerificaEmail');
+const closeVerificaEmail = document.getElementById('closeVerificaEmail');
+
+
+
+const verificaEmail = document.getElementById('verificaEmail');
 const formRegister = document.getElementById('formRegister');
+const formLogin = document.getElementById('formLogin');
+const formPrimoLogin = document.getElementById('formPrimoLogin');
+
 const home = document.getElementById('home');
 
+const forgotPassword = document.getElementById('forgotPassword');
 
 openRegister.addEventListener('click', () => {
     formRegister.classList.add('show');
@@ -25,3 +39,15 @@ closeLogin.addEventListener('click', () => {
     formLogin.classList.remove('show');
     home.classList.remove('blur');
 });
+
+openVerificaEmail.addEventListener('click', () => {
+    verificaEmail.classList.add('show');
+    formRegister.classList.remove('show');
+    home.classList.add('blur');
+});
+
+closeVerificaEmail.addEventListener('click', () => {
+    verificaEmail.classList.remove('show');
+    home.classList.remove('blur');
+});
+
