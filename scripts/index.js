2b330c4e025d88c7600c402bb657b7269c97b9be
Paperagialla <mattiabusato1234@gmail.com>
2024-02-01 22:15:25 +0100

    Aggiunti filtri prenotazione e fix bottoni
    Sono stati aggiunti i filtri per le camere, non sono ancora operativi.
    Altre aggiunte:
    - Fix bottoni home page, adesso quasi tutti funzionano
    - Fix text-area nella sezione contattaci
    - Fix footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-/* TODO: fixa sta merda che non si capisce niente */
-
 const openRegister = document.getElementById('openRegister');
 const closeRegister = document.getElementById('closeRegister');
 
@@ -9,46 +7,59 @@ const closeLogin = document.getElementById('closeLogin');
 const openVerificaEmail = document.getElementById('openVerificaEmail');
 const closeVerificaEmail = document.getElementById('closeVerificaEmail');
 
-
-
 const verificaEmail = document.getElementById('verificaEmail');
 const formRegister = document.getElementById('formRegister');
 const formLogin = document.getElementById('formLogin');
 const formPrimoLogin = document.getElementById('formPrimoLogin');
 
 const home = document.getElementById('home');
+const body = document.getElementById('body');
+
+const goToLogin = document.getElementById('goToLogin');
+const goToRegister = document.getElementById('goToRegister');
+const openFirstLogin = document.getElementById('openFirstLogin');
 
 const forgotPassword = document.getElementById('forgotPassword');
+
+const goToPrenota = document.getElementById('goToPrenota');
 
 openRegister.addEventListener('click', () => {
     formRegister.classList.add('show');
     home.classList.add('blur');
+    body.classList.add('overflow-hidden');
 });
 
 closeRegister.addEventListener('click', () => {
     formRegister.classList.remove('show');
     home.classList.remove('blur');
+    body.classList.remove('overflow-hidden');
 });
 
 openLogin.addEventListener('click', () => {
     formLogin.classList.add('show');
     home.classList.add('blur');
+    body.classList.add('overflow-hidden');
 });
 
 closeLogin.addEventListener('click', () => {
     formLogin.classList.remove('show');
     home.classList.remove('blur');
+    body.classList.remove('overflow-hidden');
 });
 
 openVerificaEmail.addEventListener('click', () => {
     verificaEmail.classList.add('show');
     formRegister.classList.remove('show');
     home.classList.add('blur');
+    body.classList.add('overflow-hidden');
 });
 
-closeVerificaEmail.addEventListener('click', () => {
+/* TODO: questo si apre dopo che l'utente ha confermato la mail inserendo la password temporanea*/
+openFirstLogin.addEventListener('click', () => {
     verificaEmail.classList.remove('show');
-    home.classList.remove('blur');
+    formLogin.classList.add('show');
+    home.classList.add('blur');
+    body.classList.add('overflow-hidden');
 });
 
 
@@ -59,3 +70,25 @@ Date.prototype.toDateInputValue = (function(){
 });
 
 document.getElementById('birthdate').max = new Date().toDateInputValue();
+
+
+goToLogin.addEventListener('click', () => {
+    formRegister.classList.remove('show');
+    formLogin.classList.add('show');
+});
+
+goToRegister.addEventListener('click', () => {
+    formLogin.classList.remove('show');
+    formRegister.classList.add('show');
+});
+
+/* TODO: sistema il recupero della password */
+forgotPassword.addEventListener('click', () => {
+    formLogin.classList.remove('show');
+    verificaEmail.classList.add('show');
+});
+
+/* TODO: pk non va sto cazzo di bottone????? */
+goToPrenota.addEventListener('click', () => {
+    window.location.href = 'prenotazione.html';
+});
